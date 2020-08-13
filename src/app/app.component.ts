f69fceb18d532d04d53f31524a15d48f13cdeb86
Fernando <fernando.schiariti@gmail.com>
2020-08-13 15:29:05 -0300

    todo and todoitem component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+//  title = 'angular-crash-todolist';
+  name: string = 'fer';
+
+  constructor() {
+      console.log(123);
+
+  }
+
 }
